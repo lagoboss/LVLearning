ff3994b6b2fea5269704ec43a11f1ca2dc6b88fd
BuildTools <unconfigured@null.spigotmc.org>
2016-12-26 09:58:38 -0600

    Added the ability to search for courses from list

--- src/com/gmail/lagoland/help/LVLearning/LVL.java
+++ src/com/gmail/lagoland/help/LVLearning/LVL.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import static sun.audio.AudioPlayer.player;
-
 /**
  * Created by lake.smith on 12/25/2016.
  */
@@ -45,28 +43,12 @@ public class LVL implements CommandExecutor{
 
     String noCoursesFound = "No courses were found... Check your config file and try again...";
     String invalidCourse = "Invalid Course... ";
+    String tooMany = "Too many arguments entered... ";
+    String tooFew = "Too few arguments entered... ";
+
+    String checking = "Checking the list...";
 
     public Boolean b;
-
-    String courseInfromation;
-
-
-    public String courseFoundIndicator(Boolean b){
-
-
-        if(b = true){
-
-            courseInfromation = ;
-
-        }
-
-        if(b = false){
-            courseInfromation = invalidCourse + noCoursesFound;
-        }
-
-        return courseInfromation;
-    }
-
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -94,7 +76,7 @@ public class LVL implements CommandExecutor{
                 return true;
             }
 
-            if(strings.length == 1){
+            else if(strings.length == 1){
 
                 if(strings[0].equalsIgnoreCase("help")){
                     List<String> messageUsage = new ArrayList<String>();
@@ -120,46 +102,58 @@ public class LVL implements CommandExecutor{
                     return true;
                 }
 
-                if(strings[0].equalsIgnoreCase("courses")){
+                else if(strings[0].equalsIgnoreCase("courses")){
 
                     if(!courses.isEmpty()){
                         for (String course : courses){
                             player.sendMessage("Course(s) include: " + course);
                         }
-
                         return true;
                     }
                     else
                         player.sendMessage(noCoursesFound);
                     return true;
                 }
-
-                if(strings[0].equalsIgnoreCase("course")){
-
-                    //getting value of 3rd string in command for later comparison to course list
-                    String arg3 = strings[1];
-
-                        for(String course : courses){
-                            if (arg3.equalsIgnoreCase(course)){
-                                player.sendMessage("Found! " + plugin.getConfig().get(""));
-                                courseFoundIndicator(true);
-                             }
-                            else{
-                                player.sendMessage("Searching...");
-                            }
-                        }
-
-
-                    return true;
-                }
-
-                return true;
             }
-        }
 
+            else if(strings.length == 2) {
+
+                if (strings[0].equalsIgnoreCase("course")) {
+
+                    player.sendMessage(checking);
+
+                    b = false;
+
+                    for (String course : courses) {
+
+                        if (strings[1].equalsIgnoreCase(course)) {
+
+                            player.sendMessage("Found! " + plugin.getConfig().getString("Traits." + strings[1]));
+
+                            b = true;
+                            //all trait names need to be lower case :/
+
+                            player.sendMessage("Description: " + plugin.getConfig().getString("Traits." + strings[1].toLowerCase() + "." + "description"));
+                            player.sendMessage("Required Permission: " + plugin.getConfig().getString("Traits." + strings[1].toLowerCase() + "." + "requirement"));
+                            player.sendMessage("Course Code: " + plugin.getConfig().getString("Traits." + strings[1].toLowerCase() + "." + "course_code"));
+
+                        } else {
+                            player.sendMessage("Searching...");
+                        }
+                    }
+
+                    if (b = false) {
+                        player.sendMessage(invalidCourse + noCoursesFound);
+
+                        return true;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+                return true;
+        }
         return true;
     }
-
-
 }
 
